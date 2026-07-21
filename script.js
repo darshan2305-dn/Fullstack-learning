0dae8660dc2f0fe3693ea4f524a92762079f35c9
Darshan Parmar <parmardarshan262@gmail.com>
2026-07-20 20:40:35 -0700

    Added first HTML, CSS and JavaScript project

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+
+    document.getElementById("message").innerHTML =
+        "🎉 Congratulations! You have successfully created your first Full Stack GitHub project.";
+
+}
