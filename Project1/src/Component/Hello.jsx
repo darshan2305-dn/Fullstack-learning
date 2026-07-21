78ff6e481f131c84cb040e1f718aa6ae70cb8f32
Darshan Parmar <parmardarshan262@gmail.com>
2026-07-20 21:21:34 -0700

    Added first React project

---
+++ Project1/src/Component/Hello.jsx
@@ -0,0 +1,12 @@
+function Hello()
+{
+    return(
+        <>
+            <div>
+                <h1>First React code</h1>
+                <b>Hello React User.</b>
+            </div>
+        </>
+    );
+}
+export default Hello;
