0525ee06e3b57984a7b44c4c155a074211b45047
Darshan Parmar <parmardarshan262@gmail.com>
2026-07-31 10:29:43 -0700

    Added first HTML, CSS and JavaScript project

--- Project1/src/App.jsx
+++ Project1/src/App.jsx
@@ -1,5 +1,3 @@
-import { createContext } from 'react'
-
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -7,31 +5,41 @@ import './App.css'
 
 import Hello from './Component/Hello'
 import Program6 from './Component/Program6'
-import User from './Component/User'
+
 import Home from './Component/Home'
 import Profile from './Component/Profile'
-import College from './Component/College'
-import Department from './Component/Department'
-import Faculty from './Component/Faculty'
+import User from './Component/User'
 import Shinchan from './assets/Shinchan.gif'
+import Sumedh from './assets/Sumedh.jpg'
 
-export const FacultyContext = createContext();
+import { createContext } from 'react'
+
+export const UserContext = createContext()
 
 function App() 
 {
-  const facultyInfo = {
-    'name' : "Jayna ma'am",
-    'email' : "jayn12@gmail",
-    'contactNo' : '+91 0934578690',
-    'city' : "Ahmedabad",
-    'image':Shinchan
-  }
+  const myArr = [
+    {
+        "name" : "Darshan",
+        "age" : 20,
+        "clg" : "JG University",
+        "course" : "MCA",
+        "img" : Shinchan
+    },
+    {
+        "name" : "Nilesh",
+        "age" : 21,
+        "clg" : "Silver Oak",
+        "course" : "BCA",
+        "img" : Sumedh
+    }
+  ]
   return(
       <>
           <div>
-              <FacultyContext.Provider value={facultyInfo}>
-                  <College/>
-              </FacultyContext.Provider>
+              <UserContext.Provider value={myArr}>
+                  <Home/>
+              </UserContext.Provider>
           </div>
       </>
   )
--- Project1/src/Component/User.jsx
+++ Project1/src/Component/User.jsx
@@ -1,32 +1,76 @@
-//import { useContext } from "react";
-//import {UserContext} from "../App";
+import { useContext } from "react";
+import {UserContext} from "../App";
 
 function User() 
 {
-  //const users = useContext(UserContext);
+  const users = useContext(UserContext);
   return (
     <>
-      <h1>User's Array List.</h1>
-      {
-            // 1st way..
-            users.map((user)=>{
-                return(
-                    <div>
-                        <h3>Name : {user.name}</h3>
-                        <h3>Age : {user.age}</h3>
-                        <hr/>
-                    </div>
-                );
-            })
-            // 2nd way..
-            // users.map((user)=>(
-            //         <div>
-            //             <h3>Name : {user.name}</h3>
-            //             <h3>Age : {user.age}</h3>
-            //             <hr/>
-            //         </div> 
-            // ))
-      }
+    <div>
+        <h1>User's Array List.</h1>
+        
+        <table border="1" align="center">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Age</th>
+                    <th>Photo</th>
+                </tr>
+            </thead>
+            <tbody>
+            {
+                // 1st way of map function              
+                users.map((user, index) => (
+                    <tr key={index}>
+                        <td>{user.name}</td>
+                        <td>{user.age}</td>
+                        <td>
+                            <img
+                              src={user.img}
+                              alt={user.name}
+                              width="100"
+                            />
+                        </td>
+                    </tr>
+                ))
+            }
+            </tbody>
+        </table>
+    </div>
+    <div>
+        <h1>User's Array List.</h1>
+        <table border="1" align="center">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>College</th>
+                    <th>Course</th>
+                    <th>Photo</th>
+                </tr>
+            </thead>
+            <tbody>
+            {
+                // 2nd way of map function
+                users.map((user, index) => {
+                    return (
+                        <tr key={index}>
+                            <td>{user.name}</td>
+                            <td>{user.clg}</td>
+                            <td>{user.course}</td>
+                            <td>
+                                <img
+                                  src={user.img}
+                                  alt={user.name}
+                                  width="100"
+                                />
+                            </td>
+                        </tr>
+                    );
+                })
+            }
+            </tbody>
+        </table>
+    </div>
     </>
   );
 }
